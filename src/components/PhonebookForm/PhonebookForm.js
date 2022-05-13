53ba99a829c8c257df60f31082b6c02dd94b1aa9
dariaddz <dariadz@gmail.com>
2022-05-13 11:49:32 +0300

    add contact

--- src/components/PhonebookForm/PhonebookForm.js
+++ src/components/PhonebookForm/PhonebookForm.js
@@ -63,7 +63,7 @@ function PhonebookForm() {
             value={name}
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            // required
+            required
             onChange={onHandleChange}
           />
         </label>
@@ -76,7 +76,7 @@ function PhonebookForm() {
             value={number}
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            // required
+            required
             onChange={onHandleChange}
           />
         </label>
